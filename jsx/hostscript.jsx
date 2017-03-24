f26880846eed3f1aa4f4cb98fa79597d55a3b8c1
CfNb <ppimac3@PPiMac3s-iMac.local>
2017-03-24 16:00:25 -0500

    weekly commit

--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -1,16 +1,18 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global $, Folder*/
 
-function showInfo(){
+function showInfo() {
     var message = "Suite |)\n CL&D Digital Illustrator Extensions Suite\n\n\n";
     
-    //message += "|) Marks\n   Tools for creating standard finishing marks based on the selected die line path."
+    message += "|) Layers\n   Tools for adding standard layer sets and modifying layers.\n\n";
     
-    message += "|) Notes\n   Create, view and delete notes for the active file. Notes are categorized by customer, job and item.\n\n"
+    message += "|) Marks\n   Tools for creating standard finishing marks based on the selected die line path.\n\n";
+    
+    message += "|) Notes\n   Create, view and delete notes for the active file.\n\n";
      
-    message += "|) Web\n   Browse the CL&D Digital reference website from within Illustrator. Visit the site in your browser at http://digital/digital\n\n"
+    message += "|) Web\n   Browse the CL&D Digital reference website from within Illustrator. Visit the site in your browser at http://digital/digital\n\n";
     
-    message += "\nDesigned by Rick Scheunemann"
+    message += "\nDesigned by Rick Scheunemann";
     
     alert(message);
 }
