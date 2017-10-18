b62f6d49254c55d7c1b782f4092dde51dca23629
rps <rscheuenemann@clddigital.com>
2017-10-18 15:56:45 -0500

    wip commit

--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -2,17 +2,12 @@
 /*global $, Folder*/
 
 function showInfo() {
-    var message = "Suite |)\n CL&D Digital Illustrator Extensions Suite\n\n\n";
-    
-    message += "|) Layers\n   Tools for adding standard layer sets and modifying layers.\n\n";
-    
-    message += "|) Marks\n   Tools for creating standard finishing marks based on the selected die line path.\n\n";
-    
-    message += "|) Notes\n   Create, view and delete notes for the active file.\n\n";
-     
-    message += "|) Web\n   Browse the CL&D Digital reference website from within Illustrator. Visit the site in your browser at http://digital/digital\n\n";
-    
-    message += "\nDesigned by Rick Scheunemann";
-    
-    alert(message);
+	var message = "Suite |)\n CL&D Digital Illustrator Extensions Suite\n\n\n";
+	message += "|) Setup\n   Tools for capturing and setting file properties and then syncing them with the database.\n\n";
+	message += "|) Layers\n   Tools for adding standard layer sets and modifying layers.\n\n";
+	message += "|) Marks\n   Tools for creating standard finishing marks based on the selected die line path.\n\n";
+	message += "|) Notes\n   Create, view and delete notes for the active file.\n\n";
+	message += "|) Web\n   Browse the CL&D Digital reference website from within Illustrator. Visit the site in your browser at http://digital/digital\n\n";
+	message += "\nDesigned by Rick Scheunemann";
+	alert(message);
 }
